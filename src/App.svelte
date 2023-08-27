893590f596c39b4905aee1c6d55803c623aa0902
Ponlawat W. <ponlawat_w@cmu.ac.th>
2023-08-27 12:17:21 +0700

    Skeleton

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { tileLayer, type Layer } from 'leaflet';
+  import Map from './components/Map.svelte';
+
+  const layers: Layer[] = [
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
+  ];
+</script>
+
+<main>
+  <Map layers={layers} />
+</main>
+
+<style></style>
---
+++ src/components/Map.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import {
+    map as initialiseMap,
+    type Layer,
+    type Map as LeafletMap
+  } from 'leaflet';
+  import { onMount } from 'svelte';
+
+  export let layers: Layer[] = [];
+
+  let map: LeafletMap;
+  
+  onMount(() => {
+    map = initialiseMap('map');
+    map.setView([18.78911, 98.98514], 13);
+    for (const layer of layers) {
+      map.addLayer(layer);
+    }
+  });
+</script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+
+<div id="map"></div>
+
+<style>
+  #map {
+    width: 100%;
+    height: 90vh;
+  }
+</style>
